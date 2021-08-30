fa230b5a8a577e0fefa1ab8d93dd50fcbab98bec
vasudevarao963 <vasudevarao.ampavilli@gmail.com>
2021-08-30 17:24:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mediaupload/ServerResponse.kt
@@ -0,0 +1,14 @@
+package com.example.mediaupload
+
+import android.content.Context
+import com.google.gson.annotations.SerializedName
+
+internal class ServerResponse {
+    fun getMessage(): Unit = Unit
+
+    fun getSuccess(): Boolean {}
+
+
+
+
+
